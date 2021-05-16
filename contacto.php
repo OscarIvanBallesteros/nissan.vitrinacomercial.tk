c81113f639e23e60c41ea470494243f48a956e7f
Oscarivan <obretrowear@gmail.com>
2021-05-15 23:22:23 -0500

    formulario arreglos

--- contacto.php
+++ contacto.php
@@ -1,14 +1,34 @@
 <?php
-    $destino= "webmaster@vitrinacomercial.tk";
-    $name = $_POST["Nombre"];
-    $email = $_POST["Email"];
-    $phone = $_POST["Teléfono"];
-    $comment = $_POST["Mensaje"];
-    $contenido = "Nombre: " $name . "\Email: " $email "\Teléfono: " $phone "\Mensaje: " $comment;
-    mail($destino, "Contacto", $contenido);
-?>
+
+function validar_campo($campo) {
+    $campo = trim($campo);
+    $campo = stripcslashes($campo);
+    $campo = htmlspecialchars($campo);
+
+    return $campo;
+}
+
+    if (isset($_POST['name']) && !empty($_POST['name']) &&
+        isset($_POST['email']) && !empty($_POST['email']) &&
+        isset($_POST['phone']) && !empty($_POST['phone']) &&
+        isset($_POST['comment']) && !empty($_POST['comment'])) {
+
+        $destinoMail = "webmaster@vitrinacomercial.tk";
 
 
+        $name = validar_campo($_POST["name"]);
+        $email = validar_campo($_POST["email"]);
+        $phone = validar_campo($_POST["phone"]);
+        $comment = validar_campo($_POST["comment"]);
+
+        $contenido = "Nombre: " .$name. "\n Email: " .$email. "\n Teléfono: " .$phone. "\n Mensaje: " .$comment;
+
+        mail($destinoMail, "Mensaje de contacto del cliente" . $name, $contenido);
+
+            return print("Ok se envió");
+        }
+    
+        return print("No se puede enviar");
 
 
 
